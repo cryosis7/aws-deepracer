d259397342f33e33dac91acbf6b062d45a8dc612
Scott Curtis <scott.curtis@hotmail.com>
2020-11-12 19:33:05 +1300

    update

--- sandbox.py
+++ sandbox.py
@@ -1,12 +1,13 @@
-import steering_based_reward as deepRacer
+import steering_speed_based_reward as deepRacer
 
 '''
 A function for testing the reward function with specific paramters
 '''
 
+
 def test():
-    params = {'x': 0, 'y': 0, 'heading': 15, 'closest_waypoints': [0, 1], 'waypoints': [
-        (0, 0), (2, 0), (4, 1), (5, 2), (4, 3), (2, 3), (1, 2), (0, 1)], 'steering_angle': -15, 'track_width': 1}
+    params = {'x': 3, 'y': 3, 'heading': -155, 'closest_waypoints': [0, 1], 'waypoints': [(0, 0), (2, 0), (4, 1), (5, 2), (4, 3), (2, 3), (1, 2), (0, 1)], 'steering_angle': 30, 'track_width': 1, 'progress': 0}
+
     print(deepRacer.debug(params))
 
 
--- test_reward_function.py
+++ test_reward_function.py
@@ -1,4 +1,4 @@
-import steering_based_reward as deepRacer
+import steering_speed_based_reward as deepRacer
 
 
 '''
@@ -10,31 +10,34 @@ in a console, cd here and run 'pytest'
 
 def setupParams():
     return {
-        'x': 0,
-        'y': 0,
+        'x': 3,
+        'y': 3,
         'heading': 0,
         'closest_waypoints': [0, 1],
         'waypoints': [(0, 0), (2, 0), (4, 1), (5, 2), (4, 3), (2, 3), (1, 2), (0, 1)],
         'steering_angle': 0,
-        'track_width': 1}
+        'track_width': 1,
+        'progress': 0}
 
 
 def test_iterate_through_all_locations():
     params = setupParams()
 
-    for x in range(-2, 7):
-        params['x'] = x
-        for y in range(-2, 5):
-            params['y'] = y
-            for i in range(0, 7):
-                params['closest_waypoints'] = [i, i + 1]
-                for heading in range(-180, 181, 5):
-                    params['heading'] = heading
-                    for steering_angle in range(-30, 31, 5):
-                        params['steering_angle'] = steering_angle
+    # for x in range(-2, 7):
+    #     params['x'] = x
+    #     for y in range(-2, 5):
+    #         params['y'] = y
+    for i in range(0, 7):
+        params['closest_waypoints'] = [i, i + 1]
+        for heading in range(-180, 181, 5):
+            params['heading'] = heading
+            for steering_angle in range(-30, 31, 5):
+                params['steering_angle'] = steering_angle
+                for progress in [0, 25, 50, 75, 100]:
+                    params['progress'] = progress
 
-                        reward = deepRacer.reward_function(params)
-                        if reward < 0 or reward > 1:
-                            print("Error: params:", params)
-                        assert reward >= 0
-                        assert reward <= 1
+                    reward = deepRacer.reward_function(params)
+                    if reward <= 0 or (reward > 1 and (reward != 30 or progress != 100)):
+                        print("Error: params:", params)
+                    assert reward > 0
+                    assert reward <= 1 or (reward == 30 and progress == 100)
